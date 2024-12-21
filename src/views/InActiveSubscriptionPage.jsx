2af068df2010bf4797f250388daab1fb1844b005
Salifu Mutaru <salifu.solutions@gmail.com>
2024-12-21 12:20:04 +0000

    fix renew

--- src/views/InActiveSubscriptionPage.jsx
+++ src/views/InActiveSubscriptionPage.jsx
@@ -42,22 +42,29 @@ export default function InActiveSubscriptionPage() {
 
       {
         user.role == "admin" ? <>
-        <h3 className="mt-4 text-center">You don't have any active Subscription! Get one by clicking below! If already subscribed, then signout and login again!</h3>
+        <h3 className="mt-4 text-center">You don't have any active Subscription! If already subscribed, then signout and login again!</h3>
         <div
         className="w-full container mx-auto grid grid-cols-1 my-20 gap-10 place-items-center px-6 lg:px-0"
       >
         <div className="rounded-2xl px-8 py-6 border flex flex-col w-full lg:w-96">
-          <h3 className="text-4xl text-green-700 font-bold text-center">{subscriptionPrice}</h3>
+          <h3 className="text-4xl text-red-700 font-bold text-center">{subscriptionPrice}</h3>
           <h3 className=" font-bold text-2xl text-center">per month</h3>
           <ul className="text-gray-700 mt-6 flex flex-col gap-2 text-start">
+            <li>✅ Monthly Renewals</li>
             <li>✅ Unlimited Orders</li>
-            <li>✅ Monthly Renewls</li>
             <li>✅ Unlimited Devices</li>
+            <li>✅ Unlimited Reservations</li>
             <li>✅ Live Kitchen Orders</li>
+            <li>✅ Unlimited Users</li>
+            <li>✅ QR Code orders</li>
+            <li>✅ Inventory Management for non-menu items</li>
+            <li>✅ Customer Management</li>
+            <li>✅ Kitchen order sound feedback</li>
+            <li>✅ Real-time</li>
           </ul>
-
-          <button onClick={btnSubscribe} className="rounded-full bg-restro-green text-white px-4 py-3 transition active:scale-95 hover:bg-restro-green-dark mt-6">Subscribe</button>
         </div>
+        <b className="text-red-500 px-4 py-3 transition active:scale-95 mt-6 text-2xl">Call your Relationship Manager to renew</b>
+
       </div>
         </>:<div>
         <h3 className="mt-4 text-center">You don't have any active Subscription!</h3>
